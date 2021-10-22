faf90f8c7d09a47d3c6df16ef974125ae6866fe9
Alexander <alexanderubilar@gmail.com>
2021-10-21 22:44:23 -0300

    first commit

---
+++ primitives/05_strings.js
@@ -0,0 +1,25 @@
+var one = '1'
+var two = 2 
+
+var result = one * two 
+
+console.log(typeof(result));
+console.log(result);
+
+// Template strings
+// Allow us to inject variables into a string in a simple way 
+// Example 
+
+// Without template string 
+var str = "Hola, este es el resultado: " + result + " con algo adcional de lo que sea"
+
+// With template strings 
+
+var str2 = `Hola, este es el resultado: ${result} con algo adicional de lo que sea`
+
+console.log(str);
+console.log(str2);
+
+var str3 = `Ejemplo con una expresión: ${1 + 2}`
+
+console.log(str3);
