ace8dad80577acfda1939609d7f90c3aee2f6cc3
Alexander <alexanderubilar@gmail.com>
2021-10-21 22:49:12 -0300

    second commit

---
+++ basics/01_variables.js
@@ -0,0 +1,49 @@
+
+// This is a comment 
+/*
+ This is a multi-line comment 
+*/  
+
+// Declare a variable with var 
+
+var someVariable 
+
+// Assign a variable 
+
+someVariable = 'some value'
+
+// Declare and assign a variable 
+
+var someOtheVariable = 'Other value'
+
+// Print to the console
+console.log(someVariable,someOtheVariable)
+
+// What happen if we print a variable not assigned 
+
+var weirdVariable
+
+console.log(weirdVariable)
+
+// Variable are declared "undefined"
+
+// What happens with an undeclared variable 
+
+// console.log(uwu) // => This is an error 
+
+// Variable name cannot start with a number 
+
+// var 2variable => This is an erro too 
+
+// Some libraries suggest to use a symbol on the name, but is not common 
+
+var $paragraph = "<p> Soy un string como html<p/>"
+var list = [1,2,3]
+
+// Variables are case sensitives 
+
+var BigNumber = 100000000
+var BIGNUMBER = 200000000 
+
+
+
